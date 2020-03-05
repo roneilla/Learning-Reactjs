64dd03fafd39ab7b1d9b999b4e474fafbd84a218
Roneilla <bumanlar@sheridancollege.ca>
2020-03-05 01:32:25 -0500

    Lesson 2
    ReactJS essentials training

---
+++ lesson2/react-is-fun/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = React.createElement(
+  "h1",
+  { id: "title", className: "header" },
+  "Hello World"
+);
+
+ReactDOM.render(title, document.getElementById("root"));
