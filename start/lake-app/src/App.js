67d95b9de7f82915ee737c088f1e4036b53daa6f
Roneilla <bumanlar@sheridancollege.ca>
2020-03-05 00:42:05 -0500

    Learning Reactjs finished

--- start/lake-app/src/App.js
+++ start/lake-app/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-function App() {
-  return (
-    <ul>
-      {lakes.map((lake) => (
-        <li key={lake.id}>
-          {lake.name}, Trailhead: {lake.trailhead}
-        </li>
-      ))}
-    </ul>
-  );
-}
+const App = ({ lakes }) => (
+  <ul>
+    {lakes.map((lake) => (
+      <li key={lake.id}>
+        {lake.name}, Trailhead: {lake.trailhead}
+      </li>
+    ))}
+  </ul>
+);
 
 export default App;
